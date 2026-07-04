6c75d37637c7a56184133a192e46ccdc473df2ee
arondlittle <littleja@my.erau.edu>
2026-07-04 17:48:06 -0500

    Add mutex for shared resource management
    Added a mutex for shared resource management and initialized it.

--- src/shared/timing_shared.cpp
+++ src/shared/timing_shared.cpp
@@ -1,2 +1,13 @@
 #include "timing_shared.h"
 volatile uint32_t g_lastHeartbeatTick = 0;
+volatile int g_system_cycle_count = 0;
+
+//Define Mutex
+SemaphoreHandle_t g_sharedResourceMutex = NULL;
+
+void InitSharedResources() {
+  //Create the mutex before tasks begins execution
+  g_sharedResourceMutex = xSemaphoreCreateMutex();
+}
+
+
